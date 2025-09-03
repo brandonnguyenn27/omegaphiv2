39b957671f66e4f9e0e7838fb8b641274cec24a4
Brandon Nguyen <brandonnguyenn27@gmail.com>
2025-09-03 15:35:55 -0700

    flask setup

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/python/:path*",
+        destination: "http://127.0.0.1:5328/:path*", // Proxy to Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
