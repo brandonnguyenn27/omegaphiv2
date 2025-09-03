863577b9168d77b18def3332ccc587355f7bdaa7
Brandon Nguyen <brandonnguyenn27@gmail.com>
2025-09-03 15:56:39 -0700

    zzzzzzz

--- src/app/api/python/index.py
+++ src/app/api/python/index.py
@@ -2,8 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/api/python/hello', methods=['GET'])
-def hello_world():
-    return "Hello, World!"
+def handler():
+   message = "Hello, World!"
+   return jsonify({"message": message})
 
 if __name__ == '__main__':
     app.run(port=5328)
